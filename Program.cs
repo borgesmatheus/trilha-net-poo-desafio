aabeee99033009eef59620a418f8704ef436e43e
Matheus de Souza Borges <matheusborgs@gmail.com>
2023-11-14 14:03:21 -0300

    Implementação de código desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Chamada do Nokia:");
+Smartphone nokia = new Nokia(numero: "3411111111", modelo: "Modelo Lumia", imei: "1965497896512", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Chamada do IPhone:");
+Smartphone iphone = new Iphone(numero: "342222222", modelo: "XR", imei: "58262591651", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tinder");
